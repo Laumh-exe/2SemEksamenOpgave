c55265c08f502a5ef7578e595a7551e512ff6abb
DavidAH <cph-dh223@cphbusiness.dk>
2023-11-27 14:14:25 +0100

    changed naming

---
+++ src/main/test/java/app/tmptest.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class tmptest
+{
+    @Test
+    public void tmp(){
+        assertTrue(false);
+    }
+}
