6a755fc5e2b4eca8b4ddadd217d453b725b232d0
DavidAH <cph-dh223@cphbusiness.dk>
2023-12-04 11:45:32 +0100

    Database is ready to be run
    made shure theat main refers to this porject's database

---
+++ doc/sql/insert.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+ALTER SEQUENCE customer_id_seq RESTART WITH 1;
+ALTER SEQUENCE itemlist_id_seq RESTART WITH 1;
+ALTER SEQUENCE item_id_seq RESTART WITH 1;
+ALTER SEQUENCE order_id_seq RESTART WITH 1;
+ALTER SEQUENCE salesperson_id_seq RESTART WITH 1;
+
+INSERT INTO customer(email,firstname,lastname,phonenumber,password) VALUES
+    ('customer@email.dk', customer, customer, 00000000, 'customer');
+INSERT INTO salesperson(email,firstname,lastname,phonenumber,password) VALUES
+    ('salesperson@email.dk', salesperson, salesperson, 00000000, 'salesperson');
+
+INSERT INTO item(prise_pr_unit,length,unit,description) VALUES
+    ('100 DK', 10, 'cm', 'nail'),
+    ('100 DK', 5, 'm', 'plank');
+
+INSERT INTO itemlist(order_id,description,quantity) VALUES
+    (1, 'box of nails', 100),
+    (1, 'some planks', 10),
+    (1, 'more nails',5);
+
+INSERT INTO item_itemlist(item_id, itemlist_id) VALUES
+    (1,1),
+    (2,2),
+    (1,3);
+
+INSERT INTO pubilc.order(status, date, customer_id, salesperson_id, total_price, carport_width, carport_length) VALUES
+    ('READY_FOR_REVIEW',2023-12-20,1,1,11500, 10, 10);
+
+END;
