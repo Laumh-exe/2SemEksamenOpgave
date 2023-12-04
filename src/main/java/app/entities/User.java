5fee3db9d59a2a4dc7cde2c12b1785364136ddd0
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-12-04 11:54:23 +0100

    User set up and UserTest
    User methods have been set up and so has the tests for them, will now start running the tests.

---
+++ src/main/java/app/entities/User.java
@@ -0,0 +1,45 @@
+package app.entities;
+
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String rank;
+    private double balance;
+
+    public User(int id, String name, String password, String rank, double balance){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.rank = rank;
+        this.balance = balance;
+    }
+
+    public User(String name){
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    public double setBalance(double newBalance){
+        balance = newBalance;
+        return balance;
+    }
+}
---
+++ src/test/java/app/entities/UserTest.java
@@ -0,0 +1,43 @@
+package app.entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getRank() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void setBalance() {
+    }
+}
