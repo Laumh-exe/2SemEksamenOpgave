7df0b67f19349d28167d42ece080ac0ad4ddd94f
DavidAH <cph-dh223@cphbusiness.dk>
2023-12-05 12:08:11 +0100

    backend order mapper works
    order mapper getallorders is working and tested

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,5 +1,12 @@
 package app.controllers;
 
+import java.util.List;
+
+import app.entities.Order;
+import app.persistence.ConnectionPool;
+
 public class OrderController {
-    
+    public static List<Order> seeAllOrders(ConnectionPool connectionPool){
+return null;
+    }
 }
--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,5 +1,45 @@
 package app.persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import app.entities.Order;
+import app.entities.OrderStatus;
+
 public class OrderMapper {
-    
+
+    public static List<Order> getAllOrders(ConnectionPool connectionPool) {
+        String sql = "SELECT * FROM orders";
+        List<Order> orders = new ArrayList<>();
+
+        try(Connection connection = connectionPool.getConnection()){
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    OrderStatus status = OrderStatus.valueOf(resultSet.getString("sataus"));
+                    Date date = resultSet.getDate("date");
+                    int customerId = resultSet.getInt("customer_id");
+                    int salespersonId = resultSet.getInt("salesperson_id");
+                    double price = resultSet.getDouble("total_price");
+                    double carportLength = resultSet.getDouble("carport_length");
+                    double carportWidth = resultSet.getDouble("carport_width");
+                    double shedLength = resultSet.getDouble("shed_length");
+                    double shedWidth = resultSet.getDouble("shed_width");
+                    
+
+                    Order order = new Order(id, date, status, price, carportLength, carportWidth, shedLength, shedWidth);
+                    orders.add(order);
+                }
+            }
+        } catch (SQLException e){
+
+        }
+        return orders;
+    }
 }
