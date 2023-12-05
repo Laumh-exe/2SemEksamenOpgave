55d02a23bc4d876dc87deb6cdcf9836ed2efbfb4
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-12-05 12:02:22 +0100

    Quick push to update from main
    Started working on usermapper and making method createUser

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,5 +1,24 @@
 package app.persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserMapper {
-    
+
+
+    public UserMapper(){
+
+    }
+
+    public void createUser(String name, String password, String rank, ConnectionPool connectionPool) throws SQLException
+    {
+        String sql= "INSERT INTO \"user\" (username, password, rank, balance) VALUES (?, ?, ?, 200)";
+        try(Connection connection = connectionPool.getConnection()){
+            try(PreparedStatement preparedStatement = connectionPool.getConnection().prepareStatement(sql)){
+                
+            }
+
+        }
+    }
 }
