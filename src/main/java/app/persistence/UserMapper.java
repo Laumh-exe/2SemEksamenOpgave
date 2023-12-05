5198c9e20b2362990512d5bb0f04cb097a08727e
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-12-05 13:23:22 +0100

    Quick fix

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -4,33 +4,31 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import app.entities.*;
 
 
 public class UserMapper {
 
 
-    public static User login(String name, String password, ConnectionPool connectionPool) throws SQLException
-    {
+    public static User login(String name, String password, ConnectionPool connectionPool) throws SQLException {
         String sql = "SELECT * FROM public.user WHERE username=? AND password=?";
 
-        try(Connection connection = connectionPool.getConnection()){
-            try(PreparedStatement preparedStatement = connectionPool.getConnection().prepareStatement(sql)){
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement preparedStatement = connectionPool.getConnection().prepareStatement(sql)) {
                 preparedStatement.setString(1, name);
                 preparedStatement.setString(2, password);
                 ResultSet resultSet = preparedStatement.executeQuery();
-                if(resultSet.next()){
+                if (resultSet.next()) {
                     int id = resultSet.getInt("id");
                     String role = resultSet.getString("role");
                     double balance = resultSet.getDouble("balance");
 
-                    return new User(id, name, password, role, balance) {
-                    } else {
-                        throw new SQLException("Fejl i login");
-                    }
+                    return new Customer(id, name, password, role, balance);
+                } else {
+                    throw new SQLException("Fejl i login");
                 }
             }
-
         }
     }
 
