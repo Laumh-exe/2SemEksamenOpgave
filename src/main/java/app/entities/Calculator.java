9390ff068100657e9200dbae537811f9c70abaa3
Laumh-exe <110572753+Laumh-exe@users.noreply.github.com>
2023-12-08 11:39:35 +0100

    Calculator laver simpel stykliste og returnerer (#40)
    * Calculator
    
    * ItemList and Item classes commit
    
    ItemList and Item classes created and has methods and fields
    
    * making tests
    
    making tests for:
    itemlist add(Item, amount)
    calculateItemList()
    
    * ItemList test passed
    
    itemlist.addTest(Item, amount) done and passed
    calculateItemlistTest done... not passed
    calculateItemList in progress
    
    * done and tested!

---
+++ src/main/java/app/entities/Calculator.java
@@ -0,0 +1,18 @@
+package app.entities;
+
+import app.entities.Carport;
+import app.entities.ItemList;
+
+public class Calculator {
+
+
+    public static ItemList calculateItemList(Carport carport) {
+        ItemList itemList = new ItemList();
+        if(carport.getLength() == 6 && carport.getWidth() == 7.8 && carport.getShed().getLength() == 6 && carport.getShed().getWidth() == 2.2) {
+            itemList.add(new Item(100, 360, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til for & bag ende"), 4);
+            itemList.add(new Item(100, 540, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til siderne"), 4);
+            itemList.add(new Item(75, 300, "Stk", "97x97 mm. trykimp. stolpe"), 11);
+        }
+        return itemList;
+    }
+}
--- src/main/java/app/entities/ItemList.java
+++ src/main/java/app/entities/ItemList.java
@@ -1,4 +1,44 @@
 package app.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemList {
+    private List<Item> itemList;
+
+    public ItemList() {
+        itemList = new ArrayList<Item>();
+    }
+
+    public void add(Item item) {
+        itemList.add(item);
+    }
+
+    public void add(Item item, int amount) {
+        for(int i = 0; i < amount; i++)
+        itemList.add(item);
+    }
+
+    public void remove(Item item) {
+        itemList.remove(item);
+    }
+
+    /*
+    public void remove(int id) {
+        for(Item item : itemList) {
+            if(item.Id() == id) {
+                itemList.remove(item);
+            }
+        }
+    }
+
+     */
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
 }
