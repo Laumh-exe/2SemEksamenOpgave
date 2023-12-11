8e32f467b63aaf6fbe7e21a3751e5610998dda7b
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-12-11 11:00:53 +0100

    addItem method added
    ItemMapper can now catch throws from the controller and save them in the DB

--- src/main/java/app/persistence/ItemMapper.java
+++ src/main/java/app/persistence/ItemMapper.java
@@ -1,5 +1,28 @@
 package app.persistence;
 
+import app.model.entities.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ItemMapper {
-    
+
+    public static void addItem(double price_pr_unit, double length, String unit, String description, ConnectionPool connectionPool) throws SQLException {
+        String sql = "INSERT INTO \"item\" (price_pr_unit, length, unit, description";
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setDouble(1, price_pr_unit);
+                preparedStatement.setDouble(2, length);
+                preparedStatement.setString(3, unit);
+                preparedStatement.setString(4, description);
+                int rowsAffected = preparedStatement.executeUpdate();
+                if (rowsAffected != 1) {
+                    throw new SQLException("Fejl");
+                }
+            }
+        } catch (SQLException e) {
+            throw new SQLException("FEJL!!");
+        }
+    }
 }
