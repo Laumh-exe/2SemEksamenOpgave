ae54f5bc125ffe4f4b65a85d5aae342f9f0ec601
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-12-12 12:43:07 +0100

    Fixing merge conflict

--- src/main/java/app/model/entities/Item.java
+++ src/main/java/app/model/entities/Item.java
@@ -1,9 +1,9 @@
 package app.model.entities;
 
 public record Item(int id, double price_pr_unit, double length, String unit, String description, int quantity) {
-    /*public Item(int id, double price_pr_unit, double length, String unit, String description, int quantity){
-        this(id, price_pr_unit, length, unit, description, quantity);
+    public Item(int id, double price_pr_unit, double length, String unit, String description){
+        this(id, price_pr_unit, length, unit, description, 0);
     }
 
-     */
+
 }
