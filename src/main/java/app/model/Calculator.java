22b1c626ddf96027fb6e7cbf7d826602dafd4559
Laumh-exe <lauritzhauge@gmail.com>
2023-12-12 18:55:26 +0100

    calculator - kigger på spær

--- src/main/java/app/model/Calculator.java
+++ src/main/java/app/model/Calculator.java
@@ -1,28 +1,97 @@
 package app.model;
 
+import app.controllers.ItemController;
 import app.model.entities.Carport;
 import app.model.entities.Item;
 import app.model.entities.ItemList;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static java.lang.Math.round;
+
+/**
+ * This is a Singelton
+ */
 public class Calculator {
 
+    private List<Item> items;
+    private static Calculator instance = null;
 
-    public static ItemList calculateItemList(Carport carport) {
-        ItemList itemList = new ItemList();
-        if(carport.isShed()) {
-            if (carport.getLength() == 6 && carport.getWidth() == 7.8 && carport.getShed().getLength() == 6 && carport.getShed().getWidth() == 2.2) {
-                itemList.add(new Item(100, 360, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til for & bag ende"), 4);
-                itemList.add(new Item(100, 540, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til siderne"), 4);
-                itemList.add(new Item(75, 300, "Stk", "97x97 mm. trykimp. stolpe"), 11);
+    /**
+     * Setup of Singleton
+     * @return this will always return either a new or an existing instance of Calculator
+     */
+    public static Calculator getInstance() {
+        if (instance==null) {
+            instance = new Calculator();
+        }
+        instance.items = ItemController.getAllItems();
+        return instance;
+    }
+
+    /**
+     * Collects itemList data from several methods
+     * @param carport
+     * @return ItemList with all collected data
+     */
+    public ItemList calculateItemList(Carport carport) {
+        List<Double> spærLengths = getSpærLengths();
+        double lowestSpærLength = spærLengths.get(0);
+        double highestSpærLength = spærLengths.get(spærLengths.size());
+        List<Item> spær = calculateSpær(carport, lowestSpærLength);
+
+        return null;
+    }
+
+    /**
+     * Get "spær" lengths in a list and sort
+     * @return
+     */
+    private List<Double> getSpærLengths() {
+        ArrayList<Double> lengths = new ArrayList<>();
+        for(Item item : items) {
+            if(item.type().equalsIgnoreCase("spær")) {
+                lengths.add(item.length());
             }
         }
-        else {
-            if (carport.getLength() == 6 && carport.getWidth() == 7.8) {
-                itemList.add(new Item(100, 360, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til for & bag ende"), 4);
-                itemList.add(new Item(100, 540, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til siderne"), 4);
-                itemList.add(new Item(75, 300, "Stk", "97x97 mm. trykimp. stolpe"), 1);
-            }
+        Collections.sort(lengths);
+        return lengths;
+    }
+
+    public List<Item> calculateSpær(Carport carport, double lowestSpærLength) {
+        Math.ceil(45);
+        double carportWidthCM = carport.getWidth()*100;
+        double carportLengthCM = carport.getLength()*100;
+        //Calculate amount
+        int spærAmount = calculateAmountSpær(carportLengthCM);
+
+        //Calculate length
+        if(carportWidthCM < lowestSpærLength) {
+            //for at finde den rigtige længde: check
         }
-        return itemList;
+        //calculate lengths
+
+        //maybe recalculate amount
+
+        /*
+        Seudocode
+        CalculateSpær {
+            If under 360(lowest spær width)
+            SpærLength=(spær der er tættest på)carport width/2
+            SpærAmount= SpærAmount /2
+            Else if over max
+            Else
+            Spær amount==
+            SpærLength=carport width
+        */
+
+        return null;
+    }
+
+    public int calculateAmountSpær(double carportLengthCM) {
+        return (int)Math.ceil(carportLengthCM/60+2);
     }
 }
