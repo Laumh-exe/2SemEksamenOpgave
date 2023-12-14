25a55940fb885f76662fdad0a8f903b26fc49566
Valdemar <valde.dk@hotmail.com>
2023-12-14 10:37:37 +0100

    mellemrum rettet

--- src/test/java/app/CalculatorTest.java
+++ src/test/java/app/CalculatorTest.java
@@ -28,8 +28,8 @@ class CalculatorTest {
         // Arrange - ItemList setup
         ItemList expectedItemList = new ItemList();
         expectedItemList.add(new Item(3, 100, 360, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til for & bag ende", 4,"spær"));
-        expectedItemList.add(new Item(4, 100, 540, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til siderne", 4, "rem "));
-        expectedItemList.add(new Item(5, 75, 300, "Stk", "97x97 mm. trykimp. stolpe", 11, "stolpe "));
+        expectedItemList.add(new Item(4, 100, 540, "Stk", "25x200 mm. trykimp. Bræt| understernbrædder til siderne", 4, "rem"));
+        expectedItemList.add(new Item(5, 75, 300, "Stk", "97x97 mm. trykimp. stolpe", 11, "stolpe"));
 
         // Act
         ItemList actualItemList = Calculator.calculateItemList(standardCarport);
